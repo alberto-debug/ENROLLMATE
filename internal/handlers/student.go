a3b1f77710f7b43db659908b4bf21bd6fbdbee91
Alberto Junior <albertomjunior1@gmail.com>
2025-10-15 19:51:24 +0300

    update

--- internal/handlers/student.go
+++ internal/handlers/student.go
@@ -9,21 +9,20 @@ import (
 )
 
 // Get /students
-func GetStudent(c *gin.Context){
+func GetStudent(c *gin.Context) {
 	var students []model.Student
 	config.DB.Find(&students)
 	c.JSON(http.StatusOK, students)
 }
 
-//Post /students
-func CreateStudent(c *gin.Context){
+// Post /students
+func CreateStudent(c *gin.Context) {
 	var student model.Student
-	if err:= c.ShouldBindJSON(&student); err != nil {
-
-		c.JSON(http.StatusBadRequest, gin.H("error": err.Error()))
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	config.DB.Create(student)
 	c.JSON(http.StatusCreated, student)
 }
-
--- internal/routes/student_routes.go
+++ internal/routes/student_routes.go
@@ -1,5 +1,9 @@
 package routes
 
+import "github.com/gin-gonic/gin"
 
 
+func StudentRoutes(router *gin.Engine){
 
+	router.GET("/student", )
+}
